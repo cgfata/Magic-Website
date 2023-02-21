4a9ef44e88961ac72e81b408a4651f3826e4151c
gmone <cavevis@gmail.com>
2023-02-20 19:45:42 -0800

    Hope this stops error

--- Website/models.py
+++ Website/models.py
@@ -5,14 +5,13 @@ from sqlalchemy.sql import func
 
 
 class Inventory(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     count = db.Column(db.Integer)
-    name = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255),nullable=False)
     edition = db.Column(db.String(255), nullable=False)
-    cardnumber = db.Column(db.Integer, nullable=False)
+    cardnumber = db.Column(db.String(255))
     foil = db.Column(db.String(4))
-    discordid = db.Column(db.String(255), db.ForeignKey('discorduser.discordid'))
-    discorduser = db.relationship("DiscordUser", back_populates="inventory")
+    discordid = db.Column(db.String(255), db.ForeignKey('user.discordid'))
 
     def to_dict(self):
         return {
@@ -33,24 +32,3 @@ class User(db.Model, UserMixin):
     discordid = db.Column(db.String(255),unique=True)
     cards = db.relationship('Inventory')
 
-class Discorduser(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    discordid = db.Column(db.String(255), nullable=False, unique=True)
-    name = db.Column(db.String(255), nullable=False)
-
-class Discordserver(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    discordid = db.Column(db.String(255), nullable=False)
-    serverid = db.Column(db.String(255), nullable=False)
-    active = db.Column(db.Integer, nullable=False)
-    at = db.Column(db.Integer, nullable=False, default=0)
-
-    # Define index
-    __table_args__ = (
-        db.Index('usertoserver_idx', discordid),
-        {},
-    )
-
-    # Define foreign key
-    discorduser = db.relationship('DiscordUser', backref='discordserver')
-    discordid_fk = db.Column(db.String(255), db.ForeignKey('discorduser.discordid'))
